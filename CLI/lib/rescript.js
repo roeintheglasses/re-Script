2b0e542763dc4fe401749a183570c7a3ad9bc787
roeintheglasses <hrkjangir@gmail.com>
2024-03-26 02:23:25 +0530

    Added Local Ollama Support

--- CLI/lib/rescript.js
+++ CLI/lib/rescript.js
@@ -3,35 +3,40 @@ import prettier from "./modifiers/prettier.js";
 import webcrack from "./modifiers/webcrack.js";
 import AnthropicLLMModifier from "./llms/anthropic.js";
 import OpenAILLMModifier from "./llms/openai.js";
-
-import ora from "ora";
+import OllamaMistralLLMModifier from "./llms/ollama/mistral.js";
 
 /**
  *
  * @param {string} code
  * @param {string} model
  * @param {string} apiKey
- * @param {ora} oraLoggerInstance
+ * @param {import("ora").Ora} spinner
  * @returns
  */
-export default async function rescript(code, model, apiKey, oraLoggerInstance) {
-  oraLoggerInstance("Reversing Bundling process...");
+export default async function rescript(code, model, apiKey, spinner) {
+  spinner.color = "orange";
+  spinner.text = "Reversing Bundling process...";
   const crackedCodeInstance = await webcrack(code);
   const crackedCode = crackedCodeInstance.code;
 
-  oraLoggerInstance("Injecting Babel plugins...");
+  spinner.text = "Injecting Babel plugins...";
   const babelifiedCode = await babelTransform(crackedCode);
 
   let llmUpdatedCode = babelifiedCode;
 
-  oraLoggerInstance("Using AI model to decode logic...");
+  spinner.color = "yellow";
+  spinner.text = "Using AI model to decode logic...";
   if (model === "claude") {
     llmUpdatedCode = await AnthropicLLMModifier(babelifiedCode, apiKey);
   } else if (model === "openAI") {
     llmUpdatedCode = await OpenAILLMModifier(babelifiedCode, apiKey);
   } else if (model === "gemini") {
+  } else if (model === "ollamaMistral") {
+    llmUpdatedCode = await OllamaMistralLLMModifier(babelifiedCode);
   }
-  oraLoggerInstance("Formatting code...");
+
+  spinner.color = "green";
+  spinner.text = "Formatting code...";
   const formattedCode = await prettier(llmUpdatedCode);
 
   return formattedCode;
